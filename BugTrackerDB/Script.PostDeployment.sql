cc9cfa638c6bd83c11cd5f4e6d30964e806eed28
Joshua John Navea Genato <jngenato@psbank.com.ph>
2021-12-11 15:10:42 +0800

    Update DB

--- BugTrackerDB/Script.PostDeployment.sql
+++ BugTrackerDB/Script.PostDeployment.sql
@@ -1,9 +1,6 @@
 ﻿TRUNCATE TABLE dbo.[User]
 --IF not exists (SELECT 1 FROM dbo.[User])
 BEGIN
-	INSERT INTO dbo.[User] (FirstName, LastName)
-	VALUES ('Josh', 'Genato'),
-	('Juan', 'Dela Cruz'),
-	('Tony', 'Stark'),
-	('John', 'Smith')
+	INSERT INTO dbo.[User] (FirstName, MiddleName, LastName,UserName, Password, Email, DateCreated)
+	VALUES ('Joshua John','Navea', 'Genato', 'jngenato', 'Password@001', 'joshgenato@gmail.com', GETDATE());
 END
--- BugTrackerDB/dbo/Tables/User.sql
+++ BugTrackerDB/dbo/Tables/User.sql
@@ -2,5 +2,11 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [FirstName] NVARCHAR(50) NOT NULL, 
-    [LastName] NVARCHAR(50) NOT NULL
+    [MiddleName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [UserName] NVARCHAR(10) NOT NULL, 
+    [Password] NVARCHAR(50) NOT NULL, 
+    [Email] NVARCHAR(50) NOT NULL, 
+    [DateCreated] DATETIME NOT NULL, 
+    [DateUpdated] DATETIME NULL
 )
